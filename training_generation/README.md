fd17c434c3a0ffae53634c137c92bea9b31ec6ab
annegriddl <anne.gritto@campus.lmu.de>
2023-06-16 18:30:04 +0200

    readme for finetuning

--- training_generation/README.md
+++ training_generation/README.md
@@ -2,14 +2,14 @@
 
 ## Data & Generation of synthetic data
 
-We use the sarcasm detection dataset from the ([*SemEval-2022 Task 6*](https://aclanthology.org/2022.semeval-1.111/)). The train set includes over two thousand self-disclosed instances of sarcasm being shared on Twitter.
+We use the sarcasm detection dataset from the [*SemEval-2022 Task 6*](https://aclanthology.org/2022.semeval-1.111/). The train set includes over two thousand self-disclosed instances of sarcasm being shared on Twitter.
 
 
 ### Data processing
 Data processing on the sarcasm data:
-    * Encoded and decoded in unicode.
-    * Tokenization, label preprocessing and column formatting.
-    * Split the dataset into train, validation, and test (Size of train set by default 0.9).
+    - Encoded and decoded in unicode.
+    - Tokenization, label preprocessing and column formatting.
+    - Split the dataset into train, validation, and test (Size of train set by default 0.9).
 
 ### Data structure
 Base dataset: ``data/{construct}/base.json``
@@ -24,3 +24,21 @@ The texts that OpenAI outputs are often messy. We conduct some simple cleaning i
 * Remove hashtags (OpenAI might add too many) (not convinced by)
 * Remove numbers ex. (1) or 2..
 * Sometimes the model replies with "Sure, here are ten...". To account for this, we remove all messages where the model agrees to our task.
+
+## Fine-tuning the model
+
+We fine-tune an E5-base model on the synthetic data. This model
+was originally trained using a contrastive loss and achieves
+strong performance in a fine-tuned classification setting.
+We fine-tune our model across two different settings: one-
+step (baseline), two-step (SAM). In the baseline setting we
+use a AdamW. In the SAM-setting we fine-tuned using the
+AdamW as the base optimizer and the two additional training
+steps (refer above). The code for the [*SAM*](https://github.com/
+davda54/sam) optimizer was taken from an online repository. 
+
+The code for fine-tuning the model, both with SAM and without, can be found in ``src/finetuning.py``. Whether to use or not to use SAM as optimizer can be specified in ``src/trainer/trainer.yaml`` by setting setting the boolean *SAM_ACTIVE* to either ``True`` or ``False``, respectively.
+
+### Running the file
+
+To run ``finetuning.py``: 
--- training_generation/src/run_inference.py
+++ training_generation/src/run_inference.py
@@ -23,45 +23,76 @@ from utils import parse_output
 import hydra
 import logging
 
-load_dotenv()
+load_dotenv() # Load environment variables
 logger = logging.getLogger(__name__)
 
 
 def generate_data(idx: int, input_text: str, polarize_directions: List[str], dataset: pd.DataFrame,
                   cfg: AugmentConfig) -> pd.DataFrame:
-    df = pd.DataFrame(columns=[f"text", "target", "labels", f"augmented_text"])
+    """
+    Generates synthetic data for each polarization direction.
 
+    Parameters:
+    idx (int): The index of the original text in the dataset.
+    input_text (str): The original text used to generate the synthetic data.
+    polarize_directions (List[str]): A list of directions to polarize the text.
+    dataset (pd.DataFrame): The original dataset.
+    cfg (AugmentConfig): The configuration object for the data generation process.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the original and generated text, the polarization direction and the target label.
+    """
+
+    # Create an empty DataFrame with specified columns
+    df = pd.DataFrame(columns=[f"text", "target", "labels", f"augmented_text"])
+    # Loop over all polarization directions
     for direction in polarize_directions:
+        # If in debug mode, stop generating after 40 iterations
         if cfg.experiment.debug:
             if idx >= 40:
                 break
-
+        
+        # Try generating new data using the PromptModule
         try:
             generator = PromptModule(cfg, input_text, **{"direction": direction})
             final_output = generator.generate()
+        # If the request times out, log an error message and continue to the next iteration
         except openai.error.Timeout as e:
             logger.error("OpenAI request timed out. Skipping this entry.")
             continue
-
+        # Parse the output from the generator
         augmented_text = parse_output(input_string=final_output)
 
+        # Create a new DataFrame with the generated data
         pl = pd.DataFrame(augmented_text, columns=[f"augmented_text"])
         pl["text"] = input_text
         pl["labels"] = direction
         pl["target"] = dataset[cfg.experiment.label_name][idx]
-
+        # Concatenate the new DataFrame with the existing one
         df = pd.concat([df, pl], ignore_index=True, axis=0)
     return df
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="inference_root.yaml")
 def main(cfg: AugmentConfig) -> None:
+    """
+    The main function of the script, responsible for managing the data generation process.
+
+    The function loads the original dataset, initializes a thread pool, submits data generation tasks to the thread pool,
+    waits for the tasks to complete, concatenates the results and saves the final dataset to a JSON file.
+
+    Parameters:
+    cfg (AugmentConfig): The configuration object for the data generation process.
+
+    Returns:
+    None
+    """
     current_time_stamp = f"{time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())}"
     start_time = time.time()
     augmentation_templates = DataTemplates()
 
     task = cfg.experiment.task
-
+    # Load the base dataset
     dataset = pd.read_json(os.path.join(cfg.file_dir, "base.json"))
 
     polarize_directions = cfg.experiment.directions
@@ -70,14 +101,17 @@ def main(cfg: AugmentConfig) -> None:
     logger.info(f"Generating data using {cfg.experiment.num_threads} threads across {num_keys} keys.")
 
     logger.info(f"Dataset is {len(dataset)} rows")
+    # Initialize a ThreadPoolExecutor
     with ThreadPoolExecutor(max_workers=num_keys*cfg.experiment.num_threads) as executor:
+        # Submit tasks to the executor
         futures = [executor.submit(generate_data, idx, input_text, polarize_directions, dataset, cfg)
                    for idx, input_text in dataset["text"].items()]
-
+    # Calculate and log the execution time
     rounded_time = round(time.time() - start_time)
     content = f"Execution time: {rounded_time} (s)"
     logger.info(content)
 
+    # Calculate and log the execution time
     df = pd.concat([future.result() for future in futures], ignore_index=True, axis=0)
 
     df = df.reset_index(drop=True)
@@ -88,6 +122,6 @@ def main(cfg: AugmentConfig) -> None:
         indent=2
     )
 
-
+# Entry point for the script
 if __name__ == "__main__":
     main()
